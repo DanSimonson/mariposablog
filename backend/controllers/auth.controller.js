6fc8fd9130ce1411e72c465b43e04b6c410c60ea
DanSimonson <dansimonson@mariposaweb.net>
2024-06-03 10:07:28 +0800

    added ability to sign in with authentication

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -49,11 +49,8 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, "Invalid password"));
     }
-    //isAdmin: validUser.isAdmin
-    const token = jwt.sign(
-      { id: validUser._id  },
-      process.env.JWT_SECRET
-    );
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
     const { password: pass, ...rest } = validUser._doc;
 
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -42,15 +42,17 @@ export default function SignUp() {
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-red-500 via-white-500 to bg-blue-500 rounded-xl text-white">
-              Mariposa Blog
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Sahand's
             </span>
+            Blog
           </Link>
           <p className="text-sm mt-5">
-            Useful information from a Software Developer's point of view. Sign
-            up with your email and password or with Google.
+            This is a demo project. You can sign up with your email and password
+            or with Google.
           </p>
         </div>
         {/* right */}
@@ -100,6 +102,18 @@ export default function SignUp() {
             </Button>
             <OAuth />
           </form>
+
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/signin" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
