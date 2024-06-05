3ff5f79257a705ccf2a69d52f68105cb9733a061
DanSimonson <dansimonson@mariposaweb.net>
2024-06-06 06:19:57 +0800

    changed signup page

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -56,7 +56,7 @@ export default function SignIn() {
           </Link>
           <p className="text-sm mt-5">
             Useful information from a Software Developer's point of view. Sign
-            in with your email and password or with Google.
+            in with your email and password.
           </p>
         </div>
 
@@ -94,7 +94,7 @@ export default function SignIn() {
                 "Sign In"
               )}
             </Button>
-            <OAuth />
+            {/* <OAuth /> */}
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Don't have an account?</span>
