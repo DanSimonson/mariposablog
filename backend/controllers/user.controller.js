e876f5b58e6336b1a0a6aa56da2d607128b62292
DanSimonson <dansimonson@mariposaweb.net>
2024-06-09 19:45:22 +0800

    added api route with ability to create a post and add it to mongodb

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,3 +3,6 @@
 export const test = (req, res) => {
   res.json({ message: "Backend is working" });
 };
+export const updateUser = async (req, res, next) => {
+  console.log(req.user);
+};
---
+++ backend/routes/post.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { verifyToken } from "../utils/veriyUser.js";
+import { create } from "../controllers/post.controller.js";
+const router = express.Router();
+
+router.post("/create", verifyToken, create);
+
+export default router;
